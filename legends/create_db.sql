42174dc8bbfedd10b3e16de336e24a9640b30d57
jeff <jderuyter@iinet.net.au>
2014-08-07 21:30:42 +1000

    Initial commit for Legends app rewrite
    Python3, Django 1.6, Firebird 2.5
    
    Upgrade Django to version 1.7 when django-firebird supports Django's
    migrations Use South 0.8.4 in the meantime.

---
+++ legends/create_db.sql
@@ -0,0 +1,5 @@
+create database
+    '/home/jeff/src/legends_site/legends.fdb'
+    default character set iso8859_1 collation en_us
+;
+commit;
---
+++ legends/settings.py
@@ -0,0 +1,94 @@
+"""
+Django settings for legends project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '&&kkxs=@%2^s-++fdby-wagu0po7#@p%)0j@=idkh6!_*yyqe='
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+)
+
+# Dev apps
+if DEBUG:
+    INSTALLED_APPS += (
+        'django_extensions',
+    )
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'legends.urls'
+
+WSGI_APPLICATION = 'legends.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE' : 'firebird',
+        'NAME' : 'legends3',      # Path to database or db alias
+        'USER' : 'legends',       # Your db user
+        'PASSWORD' : 'legends',   # db user password
+        'HOST' : '127.0.0.1',     # Your host machine
+        'PORT' : '3050',          # If is empty, use default 3050
+        'OPTIONS' : {'charset':'ISO8859_1'}
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
